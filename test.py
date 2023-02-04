7eb9fb2f238601c0da52872ee4b1c69fdbdd8936
AlejandroMolinosEligio <alejandromoeli@gmail.com>
2023-02-04 16:41:02 +0100

    Datos entrada y salidas

--- test.py
+++ test.py
@@ -6,18 +6,35 @@ def main():
     archivo_excel = pd.read_excel(file_name, skiprows=range(1,2))
     #print(archivo_excel.columns)
 
+    file = open('./DatosEntrada.txt',"w")
+
     """
     values = archivo_excel['Age'].values
     print(values)
 
+    """
     columnas = ['Age', 'Sex', 'DM']
     df_seleccionados = archivo_excel[columnas]
-    print(df_seleccionados)
-    """
+    
+    ### VALOR DE OUTPUT INDEX 24
+    # output = lista[24]
+    ### RESTO DE VALORES MENOS OUTPUT
+    # values = lista[:24]+lista[25:]
+    outputs = []
+    for i in range(364):
+        lista = archivo_excel.loc[i].values.flatten().tolist()
+        values = lista[:24]+lista[25:]
+        outputs.append(lista[24])
+        file.write(str(values)[1:-1]+"\n")
+    
+    file.close()
+
+    file = open('./Outputs.txt',"w")
+    for out in outputs:
+        file.write(str(out)+"\n")
+    file.close()
+
 
 if __name__ == "__main__":
 
-    b=3
-    a = 1 if b<2 else 2
-
-    print(a)
+    main()
